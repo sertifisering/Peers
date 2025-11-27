ca49b4ad6106c925ccbdc529b9c4c7ac078442a6
YONGGI_JEONG <121360016+sertifisering@users.noreply.github.com>
2025-11-27 11:01:01 +0100

    feature/refactor-core add recoil (#16)

---
+++ client/src/types/EventItem/index.ts
@@ -0,0 +1,3 @@
+export interface EventItem {
+  id: string;
+}
---
+++ client/src/utils/recoil/eventAtoms.ts
@@ -0,0 +1,8 @@
+import { atom, selector } from "recoil";
+// import type { EventItem } from "@/types/EventItem";
+
+//
+// export const eventListState = atom<EventItem[]>({
+//   key: "eventListState",
+//   default: [],
+// });
